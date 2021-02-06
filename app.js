226910a8488e446003ac828fd8f5f5954b866ca1
ori <ori_larusa@hotmail.com>
2021-02-06 18:30:54 -0300

    estoy comitiando cualquier cosa

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var adminRouter = require('./routes/admin');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views')); // le dice a reder que esta posicionado en views
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
@@ -39,20 +39,20 @@ app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
